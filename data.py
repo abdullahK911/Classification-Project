6721541feb6303462c978b1f526767088c3a74c4
abdullahK911 <abdullah.ahmd378@gmail.com>
2024-08-21 21:08:32 +0500

    forth commit

---
+++ data.py
@@ -0,0 +1,41 @@
+# dataset
+from utils import ImageFolder, Dataset, DataLoader
+
+import torchvision.transforms as transforms
+class ImageData(Dataset):
+  def __init__(self, dir, transform=None):
+    self.data = ImageFolder(dir, transform=transform)
+
+  def __len__(self):
+    return len(self.data)
+
+  def __getitem__(self, idx):
+    return self.data[idx]
+
+
+transform = transforms.Compose([
+    transforms.Resize((200,200)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+])
+
+trainset = ImageData(dir=r"/home/abdullah/Desktop/celeb-classification/dataset/train", transform=transform)
+validset = ImageData(dir=r"/home/abdullah/Desktop/celeb-classification/dataset/validation", transform=transform)
+testset = ImageData(dir=r"/home/abdullah/Desktop/celeb-classification/dataset/test", transform=transform)
+
+trainloader = DataLoader(trainset, batch_size=8, shuffle=True)
+validloader = DataLoader(validset, batch_size=8, shuffle=False)
+testloader = DataLoader(testset, batch_size=8, shuffle=False)
+
+
+print(trainloader)
+
+print(validloader)
+
+print(testloader)
+
+for images, labels in trainloader:
+  print(images.shape)
+  print(labels.shape)
+  break
+
---
+++ inference.py
@@ -0,0 +1,28 @@
+from utils import torch, torchvision, plt, Image
+from model import Model
+from data import testloader
+
+classes = ["anne-hathaway", "blake-lively", "brad-pitt", "christian-bale", "emily-blunt", "james-bond",
+"jennifer-garner", "leonardo-dicaprio", "margot-robbie", "robert-downey-jr"]
+
+# loading model
+model = Model()
+model.load_state_dict(torch.load(r"/home/abdullah/Desktop/celeb-classification/model.pth"))
+model.eval()
+
+
+# test.py
+correct_pred = {classname: 0 for classname in classes} # Initialize as dictionaries
+total_pred = {classname: 0 for classname in classes}
+with torch.no_grad():
+  for images, labels in testloader:
+    outputs = model(images)
+    _, predictions = torch.max(outputs.data, 1)
+    for prediction, label in zip(predictions, labels):
+      if label == prediction:
+        correct_pred[classes[label.item()]] += 1
+      total_pred[classes[label.item()]] += 1
+
+for classname, correct_count in correct_pred.items():
+  accuracy = 100 * float(correct_count) / total_pred[classname]
+  print(f"Accuracy for class: {classname:5s} is {accuracy:.1f} %")
